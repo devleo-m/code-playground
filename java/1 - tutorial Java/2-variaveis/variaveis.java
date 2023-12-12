a9b07960341daa06cda653f8247a2c1a04151839
dev-leomadeira <dev.leomadeira@gmail.com>
2023-12-12 10:14:27 -0300

    variaveis

--- java/1 - tutorial Java/2-variaveis/variaveis.java
+++ java/1 - tutorial Java/2-variaveis/variaveis.java
@@ -5,7 +5,7 @@ int- armazena inteiros (números inteiros), sem decimais, como 123 ou -123
 float- armazena números de ponto flutuante, com decimais, como 19,99 ou -19,99
 char- armazena caracteres únicos, como 'a' ou 'B'. Os valores de caracteres estão entre aspas simples
 boolean- armazena valores com dois estados: verdadeiro ou falso
- */
+*/
 
  public class variaveis {
     public static void main(String[] args) {
@@ -17,9 +17,11 @@ boolean- armazena valores com dois estados: verdadeiro ou falso
         int idade = 45;
         System.out.println(idade);
 
-        //variavel do tipo number float
-        Float dinheiro = 1.99f; //é necessario colocar f no final do numero quando é do tipo float
-        System.out.println(dinheiro);
+        //variavel do tipo number double e float
+        double money = 2.5;
+        Float dinheiro = 2.5f; //é necessario colocar f no final do numero quando é do tipo float
+        
+        System.out.println(dinheiro+money);
 
         //variavel do tipo char
         char sala = 'B';
@@ -29,5 +31,10 @@ boolean- armazena valores com dois estados: verdadeiro ou falso
         boolean myBool = true; // é possivel escrever o tipo true ou false
         System.out.println(myBool);
 
+        //Constantes (Final):
+        //Você pode criar constantes em Java usando a palavra-chave final. 
+        //O valor de uma constante não pode ser alterado uma vez que tenha sido 
+        //atribuído.
+        final double PI = 3.14159;
     }
- }
+}
