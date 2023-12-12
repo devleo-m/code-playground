3c33a0e66e471ab23b274ab66859dd274895f958
dev-leomadeira <dev.leomadeira@gmail.com>
2023-12-12 10:17:43 -0300

    metodos

---
+++ java/1 - tutorial Java/6-metodos/Metodos.java
@@ -0,0 +1,11 @@
+public class Metodos {
+
+    public int soma(int x, int y){
+        return x+y;       
+    }
+    public static void main(String[] args) {
+        Metodos calc = new Metodos();
+        int resultado = calc.soma(5,5);
+        System.out.println("O resultado da soma eh: "+ resultado);
+    }    
+}
