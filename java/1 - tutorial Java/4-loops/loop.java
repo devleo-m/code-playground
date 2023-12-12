daa291aed2c564e291ca025e248fd6dd0babab80
dev-leomadeira <dev.leomadeira@gmail.com>
2023-12-11 23:07:54 -0300

    loops while e for

---
+++ java/1 - tutorial Java/4-loops/loop.java
@@ -0,0 +1,14 @@
+public class loop {
+    public static void main(String[] args) {
+        //while 
+        int y = 0;
+        while (y < 5) {
+            System.out.println(y +" : Loop while!");
+            y++;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i +" Loop for");
+        }
+    }
+}
