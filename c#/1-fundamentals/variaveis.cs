06572f29c7917f368a6931d4d69fd8da29125526
dev-leomadeira <dev.leomadeira@gmail.com>
2023-12-15 23:49:29 -0300

    variaveis e entrada de usuario

--- c#/1-fundamentals/variaveis.cs
+++ c#/1-fundamentals/variaveis.cs
@@ -1,4 +1,31 @@
-console.WriteLine("Hello world!")
+using System;
+
+namespace variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 40;
+            double dinheiro = 150.50;
+            bool verdadeiro = true; //or false
+            char sala = 'b';
+            string nome = "Fulano de tal da Silva";
+
+            Console.WriteLine($"nome: {nome} idade: {idade} anos."); //Melhor forma de concatenar
+
+            const double test = 3.4567; //desta forma se cria uma constante
+
+            //-----------------------------------------------------------------------------------
+
+            //entrada de usuario;
+            Console.WriteLine("digite seu nome:");
+            string nomeUser = Console.ReadLine();
+
+            Console.WriteLine($"Ola, {nomeUser}");
 
 
+        }
+    }
+}
 
