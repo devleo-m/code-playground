3df144ad1307157d99714e2498d25117031e505b
dev-leomadeira <dev.leomadeira@gmail.com>
2023-12-16 15:40:58 -0300

    Math/ Math.Round(9.99)

---
+++ c#/1-fundamentals/6-math.cs
@@ -0,0 +1,19 @@
+//Math.Min(x, y)
+//O método pode ser usado para encontrar o valor mais baixo de x e y:Math.Min(x, y)
+
+Math.Min(5, 10);
+
+//Math.Sqrt(x)
+//O método retorna a raiz quadrada de x:Math.Sqrt(x)
+
+Math.Sqrt(64);
+
+//Math.Abs(x)
+//O método retorna o valor absoluto (positivo) de x:Math.Abs(x)
+
+Math.Abs(-4.7);
+
+//Math.Round()
+//Math.Round()arredonda um número para o número inteiro mais próximo:
+
+Math.Round(9.99);
