8597b4f6c02e37d7cd2a7cdc0039279492f9940d
dev-leomadeira <dev.leomadeira@gmail.com>
2023-12-17 18:38:33 -0300

    operador

---
+++ switf/1-fundamentos/2-operador.swift
@@ -0,0 +1,34 @@
+//***********************************************************************//
+//1. Operadores Aritméticos:
+let soma = 5 + 3 // Resultado: 8
+let subtracao = 7 - 2 // Resultado: 5
+let multiplicacao = 4 * 6 // Resultado: 24
+let divisao = 10 / 2 // Resultado: 5
+let resto = 15 % 4 // Resultado: 3
+//***********************************************************************//
+
+//2. Operadores de Comparação:
+let igual = 5 == 5 // Resultado: true
+let diferente = 3 != 4 // Resultado: true
+let maiorQue = 7 > 4 // Resultado: true
+let menorQue = 2 < 5 // Resultado: true
+let maiorOuIgual = 6 >= 6 // Resultado: true
+let menorOuIgual = 9 <= 9 // Resultado: true
+//***********************************************************************//
+
+//3. Operadores Lógicos:
+let and = true && false // Resultado: false
+let or = true || false // Resultado: true
+let not = !true // Resultado: false
+//***********************************************************************//
+
+// 4. Operador Ternário:
+let numero = 10
+let resultado = numero > 5 ? "Maior que 5" : "Menor ou igual a 5"
+// Se o número for maior que 5, resultado será "Maior que 5", senão será "Menor ou igual a 5"
+//***********************************************************************//
+    
+// 5. Operador de Concatenação de Strings:
+let saudacao = "Olá, " + "mundo!"
+// Resultado: "Olá, mundo!"
+//***********************************************************************//
