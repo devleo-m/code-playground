457fd6197f713c49278e03cdb958756c0df0a3ba
Leonardo <149954966+devleo-m@users.noreply.github.com>
2023-12-25 22:32:59 -0300

    Update hexagono.cpp

--- c++/faculdade/c++/hexagono.cpp
+++ c++/faculdade/c++/hexagono.cpp
@@ -4,10 +4,8 @@
 
 void calc_hexa(float L, float* area, float* perimetro) // exigido na questao
 {
-
 	*area = (3 * pow(L, 2) * sqrt(3)) / 2; // ponteiro da area e formula da questao p calcular a area
 	*perimetro = L * 6;					 // ponteiro perimetro
-
 }
 
 int main() {
@@ -17,13 +15,11 @@ int main() {
 
 	while (laco > 0 || laco < 10) // loop infinito.
 	{
-
 		printf("Entre com o lado do Hexagono:");  //Digitar o lado Hexagono
 		scanf_s("%f", &L);  // leia do lado hexagono
 
 		calc_hexa(L, &area, &perimetro);  // calculo hexagono com a formula *area e *perimetro da linha 7 e 8
 		printf("Area: %f\nPerimeto: %f\n", area, perimetro); // imprimir o resultado das duas formulas da questão
-
 	}
 
 	return 0;
