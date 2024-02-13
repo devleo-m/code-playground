ed751f136a130ceb5edf3aafc74e808601cba005
dev-leomadeira <149954966+devleo-m@users.noreply.github.com>
2024-02-13 18:24:04 -0300

    create aula interface

---
+++ java/1-tutorialJava/7-classes/Interface/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        MinhaClass testeInterface = new MinhaClass();
+        testeInterface.metodo1();
+        testeInterface.metodo2();
+
+    }
+}
