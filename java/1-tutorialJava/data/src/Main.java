d8435d3ba38301c9454cdef74c8de7cd7062ce14
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-02-20 23:15:03 -0300

    Create data / dia e horario

--- java/1-tutorialJava/data/src/Main.java
+++ java/1-tutorialJava/data/src/Main.java
@@ -1,6 +1,7 @@
 import java.time.LocalDate; // import the LocalDate class
 import java.time.LocalTime; // import the LocalTime class
 import java.time.LocalDateTime; // import the LocalDateTime class
+import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
 
 public class Main {
     public static void main(String[] args) {
@@ -39,5 +40,24 @@ public class Main {
         System.out.println(diaHorario);
         //2024-02-20T23:02:39.636025
         //Dia e hora printada no momento do metodo chamado
+
+        LocalDateTime myDateObj = LocalDateTime.now();
+        System.out.println("Before formatting: " + myDateObj);
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        DateTimeFormatter myFormatObj2 = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        String formattedDate = myDateObj.format(myFormatObj);
+        String formattedDate2 = myDateObj.format(myFormatObj2);
+
+        System.out.println("After formatting: " + formattedDate);
+        System.out.println("After formatting: " + formattedDate2);
+
+        //OBS:
+        //O ofPattern() método aceita todos os tipos de valores, se você quiser exibir a
+        // data e a hora em um formato diferente. Por exemplo:
+        //yyyy-MM-dd	   "1988-09-29"
+        //dd/MM/yyyy	   "29/09/1988"
+        //dd-MMM-yyyy	   "29-Sep-1988"
+        //MMM dd yyyy      "Thu, Sep 29 1988"
     }
 }
