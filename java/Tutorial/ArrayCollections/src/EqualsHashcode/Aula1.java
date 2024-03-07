c7126dd9f1478a3d39b37bdac0d913e6f4afe6ea
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-03-06 21:55:34 -0300

    Hash set - aula completa

--- java/Tutorial/ArrayCollections/src/EqualsHashcode/Aula1.java
+++ java/Tutorial/ArrayCollections/src/EqualsHashcode/Aula1.java
@@ -6,7 +6,6 @@ public class Aula1 {
         Usuario1 p2 = new Usuario1("Fulano","fulano@gmail.com");
         Usuario1 p3 = new Usuario1("Ciclano","ciclano@gmail.com");
 
-
         System.out.println(p1 == p2);
         System.out.println(p1.equals(p2));
     }
