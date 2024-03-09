8240ec8476945b3a76b33c4ae3d90b153b32d958
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-03-08 23:20:36 -0300

    Create Item.java

---
+++ java/Tutorial/OrientacaoObjeto/src/relacionamento/UmParaMuitos/Item.java
@@ -0,0 +1,12 @@
+package relacionamento.UmParaMuitos;
+
+public class Item {
+    String nome;
+    int quantidade;
+    double preco;
+    public Item(String nome, int quantidade, double preco) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+}
