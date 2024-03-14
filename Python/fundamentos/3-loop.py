3d6cf5701e1c2ef3815fbd89c396936050fd3838
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-03-13 23:45:52 -0300

    Create 3-loop.py

---
+++ Python/fundamentos/3-loop.py
@@ -0,0 +1,21 @@
+# Loop for: O loop for em Python é usado para iterar sobre uma sequência de elementos.
+# Você pode usar o loop for para iterar sobre listas, tuplas, strings, dicionários e outros
+# objetos iteráveis. A sintaxe é simples:
+#
+# for elemento in sequencia:
+    # Bloco de código a ser executado para cada elemento na sequência
+
+frutas = ["Banana", "Maça", "Pera", "Goiaba", "Uva", "Manga"]
+for key in frutas:
+    print(key)
+
+#Loop while: O loop while é usado para executar um bloco de código repetidamente enquanto uma
+#condição específica for verdadeira. A sintaxe é:
+
+# while condição:
+    # Bloco de código a ser executado enquanto a condição for verdadeira
+
+contador = 0
+while contador < 5:
+    print(contador)
+    contador+= 1
