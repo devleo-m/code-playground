60b4473f30dcfbf2752a0cd902082bae1fc885d5
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-03-13 23:46:12 -0300

    POO

---
+++ java/Tutorial/OrientacaoObjeto/src/heranca/Jogador.java
@@ -0,0 +1,6 @@
+package heranca;
+
+public class Jogador {
+    int x;
+    int y;
+}
--- java/Tutorial/OrientacaoObjeto/src/relacionamento/Atividade/Main.java
+++ java/Tutorial/OrientacaoObjeto/src/relacionamento/Atividade/Main.java
@@ -18,6 +18,6 @@ public class Main {
         System.out.println(cliente);
 
         System.out.println(cliente.valotTotal());
-        
+
     }
 }
