e220652f966693c63a0b0701578dcd6cc43c5884
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-03-15 23:17:22 -0300

    Create 4-list.py

---
+++ Python/fundamentos/4-list.py
@@ -0,0 +1,4 @@
+listaCompras = ["Mesa", "Cadeira", "Caneta", "Mouse", "Teclado", "Caderno"]
+print(listaCompras[-2])
+print(listaCompras)
+print(len(listaCompras)) #O metodo len() mostra o tamanho do objeto - 6
