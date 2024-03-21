61c67643652ee29cb7dbb828d7f857ed67584d4a
dev-leomadeira <149954966+devleo-m@users.noreply.github.com>
2024-03-21 00:09:12 -0300

    Create 4-tiposBasicos.dart

---
+++ dart/fundamentos/4-tiposBasicos.dart
@@ -0,0 +1,16 @@
+main() {
+  int n1 = 3;
+  double n2 = (-5.67).abs();
+  double n3 = double.parse("12.99");
+  num n4 = 6; //Num é pai de int e double
+
+  print(n1.abs() + n2 + n3 + n4);
+
+  n4 = 6.7;
+  print(n1.abs() + n2 + n3 + n4);
+
+  String s1 = "Bom";
+  String s2 = " dia";
+
+  print(s1 + s2);
+}
