7b139f6b8ce9544106da9d8b14f0143bf8a68f09
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-03-22 02:04:46 -0300

    Create 4-funcaoEmVariavel.dart

---
+++ dart/funcoes/4-funcaoEmVariavel.dart
@@ -0,0 +1,25 @@
+main(){
+  // tipo nome do valor
+  int Function(int, int) soma1 = somaFn;
+  print(soma1(2,3));
+
+  int Function(int, int) soma2 = (int x, int y){
+    return x + y;
+  };
+    print(soma2(20,313));
+
+  int soma3(int j, int k){
+     return somaFn(j, k);
+  }
+
+  print(soma3(1, 1));
+
+  var soma4(int q, int w){
+    return q + w;
+  }
+  print(soma4(10,10));
+}
+
+int somaFn(int a, int b){
+  return a + b;
+}
