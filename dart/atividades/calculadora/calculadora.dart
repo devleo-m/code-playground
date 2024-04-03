3add2bb0dbb3e6b3beb653723e8b9c80e5ee10f3
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-04-02 23:21:06 -0300

    Implementacao das interfaces

--- dart/atividades/calculadora/calculadora.dart
+++ dart/atividades/calculadora/calculadora.dart
@@ -23,6 +23,24 @@ class Calculadora extends StatefulWidget {
 }
 
 class _CalculadoraState extends State<Calculadora> {
+  String _output = '0';
+
+  Widget buildButton(String buttonText) {
+    return Expanded(
+      child: OutlineButton(
+        padding: EdgeInsets.all(24.0),
+        onPressed: () {},
+        child: Text(
+          buttonText,
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,11 +48,63 @@ class _CalculadoraState extends State<Calculadora> {
         title: Text('Calculadora'),
       ),
       body: Container(
-        child: Center(
-          child: Text(
-            'Aqui vai a interface da calculadora...',
-            style: TextStyle(fontSize: 24.0),
-          ),
+        child: Column(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
+              child: Text(
+                _output,
+                style: TextStyle(
+                  fontSize: 48.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(child: Divider()),
+            Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    buildButton('7'),
+                    buildButton('8'),
+                    buildButton('9'),
+                    buildButton('/'),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    buildButton('4'),
+                    buildButton('5'),
+                    buildButton('6'),
+                    buildButton('x'),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    buildButton('1'),
+                    buildButton('2'),
+                    buildButton('3'),
+                    buildButton('-'),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    buildButton('.'),
+                    buildButton('0'),
+                    buildButton('00'),
+                    buildButton('+'),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    buildButton('Limpar'),
+                    buildButton('='),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
