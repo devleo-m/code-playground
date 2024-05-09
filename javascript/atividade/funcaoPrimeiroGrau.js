6946fde623253b209e252cfa8040ed07480a5fa6
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-05-08 23:56:47 -0300

    Create funcaoPrimeiroGrau.js

---
+++ javascript/atividade/funcaoPrimeiroGrau.js
@@ -0,0 +1,8 @@
+let funcaoPrimeiroGrau = (a, b, x) =>{
+    return a * x + b;
+}
+  
+console.log(funcaoPrimeiroGrau(2, 3, 4)); // Deve retornar 2*4 + 3 = 11
+console.log(funcaoPrimeiroGrau(5, -2, 0)); // Deve retornar 5*0 - 2 = -2
+console.log(funcaoPrimeiroGrau(-1, 2, 3)); // Deve retornar -1*3 + 2 = -1
+  
