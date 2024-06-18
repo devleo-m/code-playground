8d3eece398fc64802a120778c0f203757e60fc86
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-06-17 21:46:26 -0300

    Create variaveis.gd

---
+++ gdscript/fundamentos/variaveis.gd
@@ -0,0 +1,20 @@
+#variaveis inteiras
+var num1 = 1;
+var num2 = 90;
+var num3 : int = 50;
+
+#variaveis reais/float
+var n1 = 4.1
+var n2 = 6.3
+var n3:float = 7.9 
+
+#strings
+var s1 = "Hello, "
+var s2 : String = "World!"
+
+#Boolean
+var b1 = true
+var b2 : bool = false
+
+#Printar
+print(s1 + s2);
