47febb2bfc93d35ce3c15537e34a2d30cfa75854
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-06-18 23:43:40 -0300

    Create metodos.rb

---
+++ ruby/ruby-intro/metodos.rb
@@ -0,0 +1,7 @@
+nome = "Leonardo Madeira"
+
+puts nome.length # tamanho da variavel 16
+puts nome.reverse # deixa meu nome ao contrario
+
+puts nome.upcase # deixa meu nome totalmente em MAIUSCULO
+puts nome.downcase # deixa meu nome totalmente em minusculo
