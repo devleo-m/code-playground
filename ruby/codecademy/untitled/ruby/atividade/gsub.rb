c1731bfa0cbbea18f95603e38a39fa968c631bd1
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-06-18 23:44:08 -0300

    up

---
+++ ruby/codecademy/untitled/ruby/atividade/gsub.rb
@@ -0,0 +1,12 @@
+# Crie um codigo que recebe o nome do usuario, crie um if para captar se o nome do usuario
+# tem um S no nome e troque o "s" por um "th"
+
+print "qual seu nome?"
+user_input = gets.chomp
+user_input.downcase!
+
+if user_input.include? "s"
+  user_input.gsub!(/s/, "th")
+end
+
+print user_input
