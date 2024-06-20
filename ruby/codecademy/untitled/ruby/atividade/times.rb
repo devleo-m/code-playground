454dcc0994222ceaac3f3d59cbd221b9821349bc
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-06-19 23:06:40 -0300

    Create times.rb

---
+++ ruby/codecademy/untitled/ruby/atividade/times.rb
@@ -0,0 +1,9 @@
+# O iterador .times
+# O .timesmétodo é como um loop supercompacto for: ele pode executar uma tarefa em cada item de
+# um objeto um determinado número de vezes.
+#
+# Por exemplo, se quiséssemos imprimir "HELLO, WORLD!!"dez vezes, poderíamos digitar
+
+10.times do
+  puts "HELLO, WORLD!!"
+end
