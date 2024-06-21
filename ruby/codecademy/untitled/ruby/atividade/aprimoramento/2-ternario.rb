764f652c959ddc9a1640c27eaca383d5a3e3af71
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-06-21 01:30:10 -0300

    Create 2-ternario.rb

---
+++ ruby/codecademy/untitled/ruby/atividade/aprimoramento/2-ternario.rb
@@ -0,0 +1,17 @@
+# Durante suas aventuras em Ruby, você percebeu que muitas vezes tem muitas opções
+# quando se trata de atingir qualquer objetivo. A ifafirmação não é exceção!
+#
+# Uma versão ainda mais concisa de if/ elseé a expressão condicional ternária .
+# É chamado de “ternário” porque leva três argumentos: um booleano, uma expressão
+# para avaliar se o booleano é truee uma expressão para avaliar se o booleano é false.
+#
+# A sintaxe é semelhante a esta:
+
+# boolean ? Do this if true: Do this if false
+
+# Um exemplo pode ser
+
+puts 3 < 4 ? "3 is less than 4!" : "3 is not less than 4."
+
+# Lembre-se: a ordem dos argumentos é importante e você não precisa de um endpara esta
+#  versão do if/ else.
