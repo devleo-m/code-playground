4a9d3c41296f7aa95fd231df9e47392b394a4ea0
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-06-21 01:41:31 -0300

    Update 3-case.rb

--- ruby/codecademy/untitled/ruby/atividade/aprimoramento/3-case.rb
+++ ruby/codecademy/untitled/ruby/atividade/aprimoramento/3-case.rb
@@ -25,3 +25,28 @@ case language2
   when "Ruby" then puts "Web apps!"
   else puts "I don't know!"
 end
+
+#ATIVIDADE
+# Adicionamos algum código para solicitar a entrada do usuário.
+#
+# Adicione uma case greetinginstrução (em qualquer estilo!) ao arquivo. Deve fazer o seguinte:
+#
+# Quando o usuário digitar “Inglês”, o programa deveráputs "Hello!"
+# Quando o usuário digitar “Francês”, o programa deveráputs "Bonjour!"
+# Quando o usuário digitar “Alemão”, o programa deveráputs "Guten Tag!"
+# Quando o usuário digitar “Finlandês”, o programa deveráputs "Haloo!"
+# Caso contrário, o programa deveria puts“Não conheço esse idioma!”
+# Quando terminar, certifique-se de inserir alguma entrada no terminal depois de
+# clicar no botão “Executar” para testar seu código.
+
+puts "Hello there!"
+greeting = gets.chomp
+
+# Add your case statement below!
+case greeting
+  when "English" then puts "Hello!"
+  when "French" then puts "Bonjour!"
+  when "German" then puts "Guten Tag!!"
+  when "Finnish" then puts "Haloo!"
+  else puts "I don’t know that language!"
+end
