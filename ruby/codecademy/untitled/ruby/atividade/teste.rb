3d497b929839202d3d7b62e463b9b319acb35263
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-06-20 22:52:31 -0300

    Update teste.rb

--- ruby/codecademy/untitled/ruby/atividade/teste.rb
+++ ruby/codecademy/untitled/ruby/atividade/teste.rb
@@ -52,3 +52,19 @@ when 'delete'
 else
   puts "Sorry, I didn't understand you."
 end
+
+#Comecemos pelo princípio: vamos criar um hash para armazenar nossos filmes e suas classificações. A seguir, vamos solicitar a entrada do usuário para que possamos eventualmente armazenar pares de filmes/classificações em nosso hash.
+
+
+movies = {
+  StarWars: 4.8,
+  Divergent: 4.7
+}
+
+puts "What would you like to do? "
+puts "-- Type 'add' to add a movie."
+puts "-- Type 'update' to update a movie."
+puts "-- Type 'display' to display all movies."
+puts "-- Type 'delete' to delete a movie."
+
+choice = gets.chomp
