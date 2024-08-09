f8074cbffba52f9b3ac50e6b3b547f8a0f20dd5e
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-08-09 00:46:05 -0300

    namespace

---
+++ typescript/avancado/aulas/namespace/exemplo.ts
@@ -0,0 +1,9 @@
+export namespace Saudacao{
+    export let bomDia = () => console.log("Bom dia");
+    export let boaNoite = () => console.log("Boa noite");
+    export let boaTarde = () => console.log("Boa tarde");
+}
+
+export namespace Despedida{
+    export let tchau = () => console.log("tchau");
+}
---
+++ typescript/avancado/aulas/namespace/index.ts
@@ -0,0 +1,7 @@
+import { Despedida, Saudacao } from "./exemplo";
+
+Saudacao.boaNoite(); // Boa noite!
+Saudacao.bomDia(); // Bom dia!
+Saudacao.boaTarde(); // Boa tarde!
+
+Despedida.tchau(); // tchau
